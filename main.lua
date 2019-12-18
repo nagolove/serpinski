b2750698d97cf0d5746163040de34e882b56116c
nagolove <dekar.own@gmail.com>
2019-12-18 15:58:49 +0300

    Some code.

--- main.lua
+++ main.lua
@@ -44,18 +44,29 @@ local verts = copy(defaultVerts)
 -- i < j
 function fractal(vertices, i, j)
     local a, b = vertices[i], vertices[j]
-    local norm = (a - b):perpendicular():normalizeInplace() * 30
+    local norm = (a - b):perpendicular():normalizeInplace() * 80
     print("norm", inspect(norm))
+    local delta = 30
+    local as = a + (b - a):normalizeInplace() * delta
+    local bs = b - (b - a):normalizeInplace() * delta
     local middle = a + ((b - a) / 2) + norm
     --local middle = a + norm
     print("middle", inspect(middle))
+    print("as", inspect(as))
+    table.insert(vertices, i + 1, as)
     table.insert(vertices, i + 1, middle)
+    table.insert(vertices, i + 1, bs)
 end
 
 function love.draw()
+    lg.setColor{1, 1, 1}
     for i = 1, #verts - 1 do
         local p1, p2 = verts[i], verts[i + 1]
         lg.line(p1.x, p1.y, p2.x, p2.y)
+    end
+    lg.setColor{0, 0.8, 0.1}
+    for _, v in ipairs(verts) do
+        lg.circle("line", v.x, v.y, 3)
     end
 end
 
